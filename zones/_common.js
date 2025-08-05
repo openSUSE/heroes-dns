43f6f1df53096e05dbe469aebb49954573672f08
Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>
2025-08-06 01:02:00 +0200

    Initialize commons
    Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

---
+++ zones/_common.js
@@ -0,0 +1,13 @@
+var NS_OPENSUSE = [
+    NAMESERVER_TTL("12h"),
+    NAMESERVER("ns1.opensuse.org."),  // NUE-IPX
+    NAMESERVER("ns2.opensuse.org."),  // PRG2
+    NAMESERVER("ns3.opensuse.org."),  // SLC1
+    NAMESERVER("ns4.opensuse.org."),  // PRG2
+];
+
+DEFAULTS(
+    DnsProvider(DSP_OPENSUSE),
+    NS_OPENSUSE,
+    DefaultTTL(43200),
+);
