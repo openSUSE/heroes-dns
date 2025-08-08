92c26ec891f24c3a6f489b12c33b8d62a1c2967e
Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>
2025-08-08 02:45:45 +0200

    Import opensuse.* except for .org
    Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

---
+++ zones/opensuse_co_in.js
@@ -0,0 +1,7 @@
+D("opensuse.co.in", REG_OPENSUSE,
+    SOA("@", "a.misconfigured.dns.server.invalid.", "hostmaster.opensuse.co.in.", 10800, 3600, 604800, 3600, TTL(3600)),
+    DefaultTTL(86400),
+    TXT("@", "v=spf1 ip4:91.193.113.64/27 ip4:143.186.213.0/24 ip4:147.2.0.0/16 ip4:149.44.0.0/16 ip4:195.135.220.0/23 ip6:2001:67c:2178::/48 ip6:2620:113:8044::/48 ip6:2a01:138:a004::/48 ip6:2a07:de40:401::/48 mx ~all"),
+    MX("@", 42, "mx1.opensuse.org."),
+    MX("@", 42, "mx2.opensuse.org."),
+);
---
+++ zones/opensuse_jp.js
@@ -0,0 +1,10 @@
+D("opensuse.jp", REG_OPENSUSE,
+    SOA("@", "ns1.opensuse.org.", "admin.opensuse.org.", 7200, 7200, 1209600, 86400),
+    AAAA("*", "2001:67c:2178:8::16"),
+    A("*", "195.135.221.140"),
+    AAAA("@", "2001:67c:2178:8::16"),
+    TXT("@", "v=spf1 -all", TTL(300)),
+    MX("@", 42, "mx1.opensuse.org."),
+    MX("@", 42, "mx2.opensuse.org."),
+    A("@", "195.135.221.140"),
+);
