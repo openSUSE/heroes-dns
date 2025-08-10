2e661eda30effeabb1aed907dca5e5c74c194eba
Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>
2025-08-10 16:40:48 +0200

    dnsconfig: enable recursive include
    Facilitate zones segrated through files in a subdirectory.
    
    Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

--- dnsconfig.js
+++ dnsconfig.js
@@ -1,4 +1,4 @@
 var DSP_OPENSUSE = NewDnsProvider("ioo-powerdns");
 var REG_OPENSUSE = NewRegistrar("none"); // we cannot control MarkMonitor
 
-require_glob("zones", false);
+require_glob("zones");
