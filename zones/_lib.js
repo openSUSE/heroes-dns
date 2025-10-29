6912304b45eca30b71d3fba2b04341348235cd8d
Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>
2025-10-29 17:43:52 +0100

    Add helper functions for PTR records
    Signed-off-by: Georg Pfuetzenreuter <mail@georg-pfuetzenreuter.net>

--- zones/_lib.js
+++ zones/_lib.js
@@ -12,3 +12,49 @@ function CNAME_BULK(domain, records) {
         }
     }
 };
+
+function AAAA_AND_PTR(domain, name, addrs) {
+    var fqdn = name + "." + domain + "."
+    D_EXTEND(domain, AAAA(fqdn, addrs[0]));
+    _.each(
+        addrs,
+        function(addr) {
+            D_EXTEND(REV(addr), PTR(addr, fqdn));
+        }
+    );
+};
+
+function AAAA_AND_PTR_BULK(domain, records) {
+    _.each(
+        records,
+        function(address, name) {
+            AAAA_AND_PTR(domain, name, [address]);
+        }
+    );
+};
+
+function HOSTS(domain, hostmap) {
+    _.each(
+        hostmap,
+        function(addresses, name) {
+            AAAA_AND_PTR(domain, name, addresses);
+        }
+    );
+};
+
+function REVERSE_ZONES_FOR_PREFIXES(soa, ttl, prefixes) {
+    DEFAULTS(
+        DEFAULT_BASE,
+        DefaultTTL(ttl),
+    );
+    _.each(
+        prefixes,
+        function(prefix) {
+            D(REV(prefix), REG_OPENSUSE,
+                AUTODNSSEC_ON,
+                soa
+            );
+        }
+    );
+    DEFAULTS(DEFAULT_ALL);
+};
